ad63c587fc310cb3ce40cd650d4d5272932bf790
vijayakumar.mk <vijayakumar.mk@zohocorp.com>
2019-09-11 12:50:19 +0530

    homepage added

---
+++ src/main/java/com/yesiamvj/desk/Error.java
@@ -0,0 +1,18 @@
+//$Id$
+package com.yesiamvj.desk;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Error {
+
+    @RequestMapping(value = "/error", method= RequestMethod.GET)
+    public String  process(){
+    	return " <h1> Error </h1>";
+    }
+
+}
